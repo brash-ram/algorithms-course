4097a2a5608ae4644f05fc691ec6e48e9f4a2c1f
Rudi <rudi.will.do.it@gmail.com>
2023-02-14 09:39:40 +0300

    first commit

---
+++ src/lesson1/TaskA.java
@@ -0,0 +1,43 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class TaskA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        long[] arrN = inputArray(n, sc);
+        long[] arrK = inputArray(k, sc);
+
+        for (int i = 0; i < arrK.length; ++i) {
+            if (find(arrN, arrK[i])) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+
+    private static boolean find(long[] arr, long el) {
+        int l = -1;
+        int r = arr.length;
+        while (l + 1 < r) {
+            int m = (l + r) / 2;
+            if (arr[m] > el) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        return r < arr.length && arr[r] == el;
+    }
+
+    private static long[] inputArray(int size, Scanner sc) {
+        long[] arr = new long[size];
+        for (int i = 0; i < size; ++i) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+}
